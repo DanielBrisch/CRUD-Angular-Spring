32e41587c748c2eb5d269d3f59c3b4469e11063e
DanielBrisch <danielbrisch157@gmail.com>
2023-09-18 21:48:18 -0300

    ainda sem sucesso de arruumar a API

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,23 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Course } from '../model/course';
-import { tap, first, delay } from 'rxjs';
+import { tap, first, delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  private readonly API = '/api/courses';
+  private readonly API = 'api/courses';
 
   constructor(private httpCliente: HttpClient) { }
 
   list() {
     return this.httpCliente.get<Course[]>(this.API)
     .pipe(
-      first(),
-      delay(5000),
-      tap(courses => console.log(courses))
+      first()
     );
   }
 }
