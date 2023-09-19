677ff4d4bac83b9b29c92811e161008e8fa78607
DanielBrisch <danielbrisch157@gmail.com>
2023-09-19 01:00:00 -0300

    mais e mais tentativas falhas de arrumar o spring para ele retornar os dados na web

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -8,7 +8,7 @@ import { tap, first, delay } from 'rxjs/operators';
 })
 export class CoursesService {
 
-  private readonly API = 'api/courses';
+  private readonly API = 'http://localhost:8080/api/courses';
 
   constructor(private httpCliente: HttpClient) { }
 
